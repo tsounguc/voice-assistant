988a829919344b3729810545aa47326f4c99f209
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-15 12:11:58 -0500

    create openai_service.dart file

---
+++ lib/openai_service.dart
@@ -0,0 +1,13 @@
+class OpenAIService {
+  Future<String> isArtPromptAPI(String prompt) async {
+    try {} catch (e) {}
+  }
+
+  Future<String> chatGPTAPI(String prompt) async {
+    return 'ChatGPT';
+  }
+
+  Future<String> dallEAPI(String prompt) async {
+    return 'Dall-E';
+  }
+}
